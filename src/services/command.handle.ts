cad0ee4240342234e2fa982550507bd2873bda2f
Purplex <69113871+HaloKodein@users.noreply.github.com>
2021-05-08 10:02:37 -0300

    Update command.handle.ts

--- src/services/command.handle.ts
+++ src/services/command.handle.ts
@@ -12,32 +12,32 @@ class Command {
 
   public async init(): Promise<void> {
     await fs.readdir("./src/commands", (err, files) => {
-      if (err) return console.log(err.message);
+      if (err) return console.log(err.message)
       files.forEach(async cmd => {
-        if (!cmd.endsWith(".ts")) return;
-        const command:ICommandObj = await import(`../commands/${cmd}`);
+        if (!cmd.endsWith(".ts")) return
+        const command = await import(`../commands/${cmd}`) as ICommandObj
         events.emit("LOAD_COMMAND", command.default.help.name.toLowerCase())
-        this.commands.set(command.default.help.name.toLowerCase(), command.default);
+        this.commands.set(command.default.help.name.toLowerCase(), command.default)
         command.default.help.aliases.forEach(e => {
-          this.aliases.set(e, command.default.help.name.toLowerCase());
+          this.aliases.set(e, command.default.help.name.toLowerCase())
         })
       })
     })
   }
 
   public async handle(client, message, args, command:string): Promise<void> {
-    const cmd = this.findCommand(command);
-    if (!cmd) return;
+    const cmd = this.findCommand(command)
+    if (!cmd) return
 
     try {
       cmd.execute(client, message, args);
     } catch(err) {
-      events.emit("LOAD_COMMAND_ERROR", err.message);
+      events.emit("LOAD_COMMAND_ERROR", err.message)
     }
   }
 
   public findCommand(command) {
-    return this.commands.get(command) ?? this.commands.get(this.aliases.get(command));
+    return this.commands.get(command) ?? this.commands.get(this.aliases.get(command))
   }
 }
 
