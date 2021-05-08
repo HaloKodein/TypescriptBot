3703ec7e3e05632e2d7f67151f6dbd522e4635e4
Purplex <69113871+HaloKodein@users.noreply.github.com>
2021-05-08 10:07:55 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,14 @@
 export = {
-  token: "",
+  token: process.env.TOKEN,
   owner: "",
   clientid: "",
   prefix: "",
   debug: false, //true/false
   warn: false,  //true/false
   dashboard: {
-    sessionSecret: "",
-    oauthSecret: "",
-    callbackURL: "https://localhost:3000/callback"
+    sessionSecret: process.env.SESSION_SECRET,
+    oauthSecret: process.env.OUATH_SECRET,
+    apiEndpoint: "http://localhost:3000/api"
+    callbackURL: `${this.apiEndpoint}/callback`
   }
 }
